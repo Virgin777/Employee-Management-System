ee0e2a87a3511deca5200e406a8e1ee06bd6e194
sahaya virgin mary <146453232+Virgin777@users.noreply.github.com>
2025-10-28 12:01:22 +0530

    Delete run_server.bat

--- run_server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "c:\Users\virgi\OneDrive\Desktop\Employee Management Systemsss"
-call venv\Scripts\activate
-cd backend
-echo "Running Django management command..."
-python manage.py runserver 0.0.0.0:8000
