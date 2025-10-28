c3a2e5e34ffe60b880a9639bf3bd6386f659eea0
sahaya virgin mary <146453232+Virgin777@users.noreply.github.com>
2025-10-28 12:02:35 +0530

    Delete create_employees_new.bat

--- create_employees_new.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\Users\virgi\OneDrive\Desktop\Employee Management Systemsss"
-call venv\Scripts\activate
-cd backend
-echo Creating sample employees...
-python manage.py create_sample_employees
-echo Done!
